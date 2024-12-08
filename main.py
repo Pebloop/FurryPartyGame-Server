8c124536caee8fbbc7dec7fb7bbd3cf391331c74
Pebloop <pebloop@outlook.com>
2024-12-08 04:01:52 +0100

    add dotenv

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ import ssl
 
 from websockets import ConnectionClosedOK
 from websockets.asyncio.server import serve
+from dotenv import load_dotenv
 
 from src.manage_websockets import manage_websockets
 
-
 ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-ssl_context.load_cert_chain(os.environ.get("SSL_FULCHAIN", ""), os.environ.get("SSL_PRIVKEY", ""))
+ssl_context.load_cert_chain(os.getenv("SSL_FULLCHAIN", ""), os.getenv("SSL_PRIVKEY", ""))
 
 async def handler(websocket):
     async for message in websocket:
@@ -19,8 +19,9 @@ async def handler(websocket):
 
 
 async def main():
-    port = int(os.environ.get("PORT", 8001))
-    async with serve(handler, os.environ.get("HOST", "localhost"), port, ssl=ssl_context):
+    load_dotenv()
+    port = int(os.getenv("PORT", 8001))
+    async with serve(handler, os.getenv("HOST", "localhost"), port, ssl=ssl_context):
         await asyncio.get_running_loop().create_future()
 
 
